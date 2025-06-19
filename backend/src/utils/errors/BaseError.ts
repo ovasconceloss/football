2b6d8d10a693e36a2f98e43fc670c7094facc2e3
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-06-19 21:14:14 +0100

    feat(errors): create a base error class for custom errors

---
+++ backend/src/utils/errors/BaseError.ts
@@ -0,0 +1,16 @@
+export class BaseError extends Error {
+    public readonly name: string;
+    public readonly statusCode: number;
+    public readonly isOperational: boolean;
+
+    constructor(name: string, statusCode: number, message: string, isOperational: boolean = true) {
+        super(message);
+        Object.setPrototypeOf(this, new.target.prototype);
+
+        this.name = name;
+        this.statusCode = statusCode;
+        this.isOperational = isOperational;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
