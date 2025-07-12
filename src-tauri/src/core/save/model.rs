729ed5116c02d03a0611084f5befb92bd8a7cca4
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-07-12 18:17:45 +0100

    feat: create an implementation for save
    - create a function to create a new save

--- src-tauri/src/core/save/model.rs
+++ src-tauri/src/core/save/model.rs
@@ -7,3 +7,13 @@ pub struct Save {
   pub name: String,
   pub created_at: String,
 }
+
+impl Save {
+  pub fn new(name: String) -> Self {
+    Save { 
+      uuid: Uuid::new_v4(), 
+      name: name, 
+      created_at: chrono::Utc::now().to_rfc3339() 
+    }
+  }
+}
