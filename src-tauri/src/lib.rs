1903ca9cb1a62b76197d0445848eb0cef1871593
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-07-12 19:28:57 +0100

    feat: add the new_save command in tauri

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,19 @@
-pub mod core;
-pub mod application;
-pub mod infrastructure;
+mod core;
+mod application;
+mod infrastructure;
+
+use application::commands::save::new_save;
+use crate::infrastructure::database::connection::create_connection;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+pub async fn run() {
+    let connection = create_connection().await;
+
     tauri::Builder::default()
+        .manage(connection)
+        .invoke_handler(tauri::generate_handler![
+            new_save
+        ])
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
